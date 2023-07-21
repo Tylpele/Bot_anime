5df8bad9dacc229142a5c1ce47de7a0ef624f562
Caibor <shashkoff.danil2016@yandex.ru>
2023-07-21 18:58:08 +0600

    Более-менее рабочая версия

--- data.py
+++ data.py
@@ -1,69 +1,88 @@
 import random
-
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 TOKEN_API = "6341860132:AAHeV2AAFyVZqNK73nAtbnG58GGvYRuBBpo"
 
+storage = MemoryStorage()
 bot = Bot(TOKEN_API)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 
 @dp.message_handler(commands=['start']) # что происходит при команде старт
 async def begin(message: types.Message):
-      markup = InlineKeyboardMarkup() # хрень, без которой не работает inline кнопки
-      # список кнопок
-      random_anime=InlineKeyboardButton("Хачу аниме", callback_data="random_anime")
-      anime_list=InlineKeyboardButton("Просмотренные", callback_data="anime_list")
-      see_anime = InlineKeyboardButton("Список посмотреть", callback_data="see_anime")
-      add_anime_list=InlineKeyboardButton("Добавить в список", callback_data="add_anime_list")
-      # добавление самих кнопок
-      markup.add(anime_list,add_anime_list)
-      markup.add(random_anime, see_anime)
-      await bot.send_message(message.chat.id, "Чего желаем?", reply_markup=markup) #отправка сообщения с кнопками
+      await message.reply("/watched_list - список просмотренных тайтлов \n"
+                          "/will_see - список запланированных тайтлов \n"
+                          "/random_anime - случайное аниме из запланированных \n"
+                          "/count_watched - число просмотренных тайтлов \n"
+                          "/count_will_see - число запланированных тайтлов \n"
+                          "/add_watched - добавить в просмотренные \n")
 
 
-# что происходит при нажатии кнопки "Просмотренные"
-@dp.callback_query_handler(lambda c: c.data == "anime_list")
-async def show_list(call: types.callback_query):
-      # открытие списка для чтение и вывод его
-      anime_file = open('list.txt','r', encoding="utf-8")
+@dp.message_handler(commands=["watched_list"])
+async def show_watched_list(message: types.Message):
+      anime_file = open('list.txt', 'r', encoding="utf-8")
       anime_list = anime_file.read()
-      await bot.answer_callback_query(call.id)
-      await bot.send_message(call.message.chat.id, anime_list) # бот отправляет список
+      await message.reply(anime_list)
       anime_file.close()
 
 
-@dp.callback_query_handler(lambda c: c.data=="see_anime")
-async def show_see(call: types.callback_query):
+@dp.message_handler(commands=["will_see"])
+async def show_will_see(message: types.Message):
       anime_file = open('see.txt', 'r', encoding="utf-8")
       anime_list = anime_file.read()
-      await bot.answer_callback_query(call.id)
-      await bot.send_message(call.message.chat.id, anime_list)
+      await message.reply(anime_list)
       anime_file.close()
 
-#Выдаёт рандомное аниме из списка
-@dp.callback_query_handler(lambda c: c.data=="random_anime")
-async def give_random(call: types.callback_query):
-      # открытие файла и его преобразование в список
+
+@dp.message_handler(commands=["random_anime"]) #!!!Возможно сюда приделать кнопки "добавить в просмотренное" и "ещё"
+async def give_random_anime(message: types.Message):
       anime_file = open('see.txt', 'r', encoding="utf-8")
       anime_text=anime_file.read()
       list_anime= anime_text.split('\n')
-      random_anime = random.choice(list_anime) # выбор рандомного элемента списка
-      #print(len(list_anime))
-      await bot.send_message(call.message.chat.id, random_anime) # отправка результата
+      random_anime = random.choice(list_anime)
+      await message.reply(random_anime)
       anime_file.close()
-#
-# @dp.callback_query_handler(lambda c: c.data == "btn_back")
-# async def begin(message: types.Message):
-#       markup = InlineKeyboardMarkup()
-#       random_anime=InlineKeyboardButton("Хачу аниме", callback_data="random_anime")
-#       anime_list=InlineKeyboardButton("Просмотренные", callback_data="anime_list")
-#       see_anime = InlineKeyboardButton("Список посмотреть", callback_data="see_anime")
-#       add_anime_list=InlineKeyboardButton("Добавить в список", callback_data="add_anime_list")
-#       markup.add(anime_list,add_anime_list)
-#       markup.add(random_anime, see_anime)
-#       await bot.send_message(message.chat.id, "Чего желаем?", reply_markup=markup)
+
+
+@dp.message_handler(commands=["count_watched"])
+async def count_watched_anime(message: types.Message):
+      anime_file = open('list.txt', 'r', encoding="utf-8")
+      anime_list = anime_file.read()
+      anime_list = anime_list.split(";")
+      await message.reply(len(anime_list)-1)
+      anime_file.close()
+
+
+@dp.message_handler(commands=["count_will_see"])
+async def count_will_see_anime(message: types.Message):
+      anime_file = open('see.txt', 'r', encoding="utf-8")
+      anime_text=anime_file.read()
+      list_anime= anime_text.split('\n')
+      await message.reply(len(list_anime))
+      anime_file.close()
+
+
+
+class Add_Watched(StatesGroup):
+      waiting_add_wached = State()
+@dp.message_handler(commands=["add_watched"])
+async def state_add_watched(message: types.Message, state: FSMContext):
+      await message.reply("Какое аниме я посмотрел?")
+      await state.set_state(Add_Watched.waiting_add_wached.state)
+
+@dp.message_handler(state=Add_Watched.waiting_add_wached)
+async def add_watched(message: types.Message, state: FSMContext):
+      await state.update_data(new_anime = message.text, encoding="utf-8")
+      text=message.text+"; "
+      with open('list.txt', 'a+', encoding="utf-8") as file_tee:
+            file_tee.write(f'{text}')
+      await message.answer(f"Записано")
+      await state.finish()
 
 def BOT():
     executor.start_polling(dp)
